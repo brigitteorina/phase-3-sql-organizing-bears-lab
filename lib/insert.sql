22253a786749e0e042ad0918acee17af044de337
brigitteorina <brigitteorina@gmail.com>
2023-05-28 10:43:21 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 2, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 7, 'M', 'Brown', 'Brave', 1),
+  ('Melissa', 4, 'F', 'Cinnamon', 'Sweet', 1),
+  ('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+  ('Wendy', 3, 'F', 'White', 'Energetic', 1),
+  ('Unnamed', NULL, NULL, NULL, NULL, 0);
